cc8f384b4607a9f9f54731871d0f948228ee7414
Parmesh Krishen <therealparmesh@outlook.com>
2024-12-09 09:55:42 -0600

    update packages

--- prettier.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  singleQuote: true,
-  trailingComma: 'all',
-  plugins: ['prettier-plugin-go-template', 'prettier-plugin-tailwindcss'],
-};
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./**/*.{gohtml,js}', '!./build/**', '!./**/node_modules/**'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
